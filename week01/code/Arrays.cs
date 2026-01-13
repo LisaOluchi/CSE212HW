a878716a3060d3304a4ff5b1565ae30e9d676e11
LisaOluchi <sangqulisa@gmail.com>
2026-01-13 03:55:50 +0200

    W1 Submission

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,13 +8,23 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Step 1: Construct an array to contain the multiples.
+        double[] multiples = new double[length];
 
-        return []; // replace this return statement with your own
+        // Step 2: Loop above each point in the array.
+        for (int i = 0; i < length; i++)
+        {
+            
+            // Step 3: Calculate the multiple.
+            multiples[i] = number * (i + 1);
+        
+        }
+
+        // Step 4: Bring back the array.
+        return multiples; 
+    
     }
+
 
     /// <summary>
     /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
@@ -25,9 +35,35 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+
+        // Security check for testing.
+        if (data.Count == 0)
+            return;
+
+        //Regulating amount first.
+          amount = amount % data.Count;
+
+        // Step 1: Compute the cut point.
+        int startIndex = data.Count - amount;
+
+
+        // Step 2: Obtain tail of the list, the part that seemingly laps the head.
+        List<int> endPart = data.GetRange(startIndex, amount);
+
+        // Step 3: Obtain the head of the list, the part that seemingly laps the tail.
+        List<int> beginningPart = data.GetRange(0, startIndex);
+
+
+         // Step 4: Remove the original list.
+         data.Clear();
+
+
+         // Step 5: Attach the changed pieces in the list.
+         data.AddRange(endPart);
+         data.AddRange(beginningPart);
+
+
+
+
     }
 }
